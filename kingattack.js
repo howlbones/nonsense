26eb3f11c78d476d8f199015f5e232284d1cb142
Your Namesifer <olegajun@gmail.com>
2024-07-18 23:53:25 +0300

    Add punch and kick to king

---
+++ kingattack.js
@@ -0,0 +1,34 @@
+let punchButton = document.querySelector('.king-controls_punch');
+let kickButton = document.querySelector('.king-controls_kick');
+let kingStill = document.querySelector('.king__img_still');
+let kingPunch = document.querySelector('.king__img_punch');
+let kingKick = document.querySelector('.king__img_kick');
+
+let isPunching = false;
+let isKicking = false;
+
+punchButton.addEventListener('mousedown', () => {
+  if (!isPunching) {
+    kingStill.classList.toggle('hidden');
+    kingPunch.classList.toggle('hidden');
+    isPunching = true;
+    setTimeout(() => {
+      kingStill.classList.toggle('hidden');
+      kingPunch.classList.toggle('hidden');
+      isPunching = false;
+    }, 200);
+  }
+});
+
+kickButton.addEventListener('mousedown', () => {
+  if (!isKicking) {
+    kingStill.classList.toggle('hidden');
+    kingKick.classList.toggle('hidden');
+    isKicking = true;
+    setTimeout(() => {
+      kingStill.classList.toggle('hidden');
+      kingKick.classList.toggle('hidden');
+      isKicking = false;
+    }, 200);
+  }
+});
