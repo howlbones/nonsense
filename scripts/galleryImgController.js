4f7639789f133b37e029da85a66a8c9c84c9e5d8
Your Namesifer <olegajun@gmail.com>
2024-07-20 21:59:10 +0300

    Add portraits to gallery

--- scripts/galleryImgController.js
+++ scripts/galleryImgController.js
@@ -1,7 +1,18 @@
 const images = [...document.querySelectorAll('.image-showcase__img')];
+const portraits = [...document.querySelectorAll('.portraits__img')];
+console.log('script running')
+console.log(images)
 
 images.forEach((image) => {
   image.addEventListener('click', (evt) => {
+    evt.target.classList.toggle('image-showcase__img_opened');
+    evt.stopPropagation();
+  })
+})
+
+portraits.forEach((image) => {
+  image.addEventListener('click', (evt) => {
+    console.log('yes')
     evt.target.classList.toggle('image-showcase__img_opened');
     evt.stopPropagation();
   })
