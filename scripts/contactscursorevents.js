d8d6af8accb4aa29ad7372cf07dff666a63d1ffb
Your Namesifer <olegajun@gmail.com>
2024-07-25 18:30:06 +0300

    Modify contacts page

--- scripts/contactscursorevents.js
+++ scripts/contactscursorevents.js
@@ -1,10 +1,30 @@
-const contentContainer = document.querySelector('.content-container');
+// const contentContainer = document.querySelector('.content-container');
+const blackSunEl = document.querySelector('.blackcircle');
+const sunEl = document.querySelector('.sun-container');
+// function updateMargins(event) {
+//   let styles = `
+//       .content-container { 
+//         height: ${ 900 - event.pageY}px !important;
+//       }
+//   `
+//   const styleSheet = document.createElement("style")
+//   styleSheet.textContent = styles
+//   document.head.appendChild(styleSheet)
+// }
 function updateMargins(event) {
-  let styles = `
-      .content-container { 
-        height: ${ 900 - event.pageY}px !important;
-      }
-  `
+  // let styles = `
+  //     .blackcircle { 
+  //       left: -50px;
+  //       transform: translateX(${event.pageX}px);
+  //     }
+
+  //     .sun-container { 
+  //       left: -50px;
+  //       transform: translateX(${-event.pageX}px);
+  //     }
+  // `
+  blackSunEl.style.transform = `translate(${event.pageX}px)`
+  sunEl.style.transform = `translate(${event.pageX}px)`
   const styleSheet = document.createElement("style")
   styleSheet.textContent = styles
   document.head.appendChild(styleSheet)
