65c3b5a9dbd3053608c8ae4323c5e74e54edd4c0
Your Namesifer <olegajun@gmail.com>
2024-07-26 09:19:22 +0300

    Change font

--- scripts/galleryImgController.js
+++ scripts/galleryImgController.js
@@ -1,7 +1,5 @@
 const images = [...document.querySelectorAll('.image-showcase__img')];
-const portraits = [...document.querySelectorAll('.portraits__img')];
-console.log('script running')
-console.log(images)
+// const portraits = [...document.querySelectorAll('.portraits__img')];
 
 images.forEach((image) => {
   image.addEventListener('click', (evt) => {
@@ -10,10 +8,10 @@ images.forEach((image) => {
   })
 })
 
-portraits.forEach((image) => {
-  image.addEventListener('click', (evt) => {
-    console.log('yes')
-    evt.target.classList.toggle('image-showcase__img_opened');
-    evt.stopPropagation();
-  })
-})
+// portraits.forEach((image) => {
+//   image.addEventListener('click', (evt) => {
+//     console.log('yes')
+//     evt.target.classList.toggle('image-showcase__img_opened');
+//     evt.stopPropagation();
+//   })
+// })
